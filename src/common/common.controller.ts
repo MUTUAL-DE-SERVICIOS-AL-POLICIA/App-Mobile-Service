4ccf4d15ff4f502695f438117a62fd8c7f106e5b
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-03 10:12:23 -0400

    ELIMINACIÓN DE RECORD INTERNO POR IMPLEMETACIÓN DE SERVICIO RECORDS

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,17 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { RecordService } from 'src/common/records';
 
 @Controller()
-export class CommonController {
-  constructor(private readonly recordService: RecordService) {}
-
-  @MessagePattern('appMobile.record.create')
-  create(
-    @Payload('action') action: string,
-    @Payload('description') description: string,
-    @Payload('metadata') metadata: any,
-  ) {
-    return this.recordService.create(action, description, metadata);
-  }
-}
+export class CommonController {}
