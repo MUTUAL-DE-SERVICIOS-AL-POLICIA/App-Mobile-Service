afa92a883433b034667823f2ef6fd507b835af2e
ferbautistamollo <ferbautistamollo@gmail.com>
2025-10-29 16:52:51 -0400

    obligar VERSION DE LA APP MOVIL PARA ACTUALIZACIÓN

--- src/app-mobile/app-mobile.service.ts
+++ src/app-mobile/app-mobile.service.ts
@@ -258,15 +258,15 @@ export class AppMobileService {
     const stores: Record<string, { url: string; validVersions: string[] }> = {
       playstore: {
         url: 'https://play.google.com/store/apps/details?id=com.muserpol.pvt',
-        validVersions: ['4.0.1','4.1.1'],
+        validVersions: ['4.1.1'],
       },
       appstore: {
         url: 'https://apps.apple.com/app/id284815942',
-        validVersions: ['4.0.1','4.1.1'],
+        validVersions: ['4.1.1'],
       },
       appgallery: {
         url: 'https://appgallery.huawei.com/app/C106440831',
-        validVersions: ['4.0.1','4.1.1'],
+        validVersions: ['4.1.1'],
       },
     };
 
